ac1a41b6c49fe990a29128481b6fc3e3b6ec0e13
Саша Орлов <caxa_2004@mail.ru>
2023-02-02 14:10:09 +0300

    plus

---
+++ PlusForGit/Form1.cs
@@ -0,0 +1,24 @@
+namespace PlusForGit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 0, b = 0, c = 0;
+
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+
+            c = a + b;
+
+            textBox3.Text = c.ToString();
+        }
+
+       
+    }
+}
